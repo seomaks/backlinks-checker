eea808edaf1ada8d8845686fecfc7a80fa47ee9d
seomaks <m.voevodin88@gmail.com>
2022-01-27 22:14:40 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import LinksLoader from "./components/links-checker/links-checker";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LinksLoader />
     </div>
   );
 }
 
-export default App;
+export default App
